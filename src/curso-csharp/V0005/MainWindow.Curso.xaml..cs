bbe415cce4cfeafc1d5a4a62d56d77092756f98d
Marcelo Uchimura <one234quatro@gmail.com>
2024-03-17 21:09:53 -0300

    Curso preliminar de bloco switch

---
+++ src/curso-csharp/V0005/MainWindow.Curso.xaml..cs
@@ -0,0 +1,102 @@
+﻿using NAudio.Wave.SampleProviders;
+using NAudio.Wave;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Text;
+
+namespace V0005
+{
+    partial class MainWindow
+    {
+        async Task AplicarVezDoGeniusAsync()
+        {
+            await TocarBeepAteNumeroEtapasAlcancadasMaisUmAsync();
+            VezDoGenius = false;
+            BarraMensagem.Text = "Vez do Jogador";
+            PosicaoNoVetor = 0;
+        }
+
+        async Task TocarBeepAteNumeroEtapasAlcancadasMaisUmAsync()
+        {
+            Button? botao = null;
+
+            for (int indice = 0; indice <= NumeroEtapasAlcancadas; indice += 1)
+            {
+                switch (Sequencia[indice])
+                {
+                    case 0:
+                        botao = BotaoVermelho0;
+                        break;
+                    case 1:
+                        botao = BotaoVerde1;
+                        break;
+                    case 2:
+                        botao = BotaoAzul2;
+                        break;
+                    case 3:
+                        botao = BotaoAmarelo3;
+                        break;
+                    default:
+                        throw new NotSupportedException("Botão " + Sequencia[indice] + " desconhecido.");
+                }
+
+                botao.Tag = "ShowAnimation";
+                await GerarBeepAsync(Sequencia[indice]);
+                botao.Tag = "";
+            }
+        }
+
+        async Task VerificarBotaoApertadoAsync(Button botaoApertado)
+        {
+            botaoApertado.IsEnabled = false;
+
+            await Task.Delay(100);
+
+            // 1. Dado a propriedade Name do botaoApertado,
+            //    a. Caso seja "BotaoVermelho0" quando o índice PosicaoNoVetor de Sequencia for igual a 0;
+            //    b. Caso seja "BotaoVerde1" quando o índice PosicaoNoVetor de Sequencia for igual a 1;
+            //    c. Caso seja "BotaoAzul2" quando o índice PosicaoNoVetor de Sequencia for igual a 2;
+            //    d. Caso seja "BotaoAmarelo3" quando o índice PosicaoNoVetor de Sequencia for igual a 3;
+            //       I. Habilitar o botaoApertado
+            //       II. await AcertouBotaoAsync();
+            //       III. escapa do bloco.
+            //    e. Por padrão,
+            //       I. BotaoVermelho0, BotaoVerde1, BotaoAzul2 e BotaoAmarelo3 são todos desabilitados
+            //       II. BarraMensagem contém a string "Você perdeu."
+            //       III. escapa do bloco.
+            //
+            // Retire a linha abaixo e implemente o corpo deste método.
+            throw new NotImplementedException();
+        }
+
+        async Task AcertouBotaoAsync()
+        {
+            PosicaoNoVetor += 1;
+            (PosicaoNoVetor, NumeroEtapasAlcancadas, bool irParaVezDoGenius) = PosicaoNoVetor > NumeroEtapasAlcancadas
+                ? (0, NumeroEtapasAlcancadas + 1, true)
+                : (PosicaoNoVetor, NumeroEtapasAlcancadas, false);
+
+            switch (NumeroEtapasAlcancadas)
+            {
+                case TotalEtapas:
+                    BotaoVermelho0.IsEnabled = BotaoVerde1.IsEnabled = BotaoAzul2.IsEnabled = BotaoAmarelo3.IsEnabled = false;
+                    BarraMensagem.Text = "Você venceu!";
+                    return;
+                default:
+                    break;
+            }
+
+            switch (irParaVezDoGenius)
+            {
+                case true:
+                    VezDoGenius = true;
+                    BarraMensagem.Text = "Vez do Genius";
+                    await AplicarVezDoGeniusAsync();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ src/curso-csharp/V0005/MainWindow.xaml.cs
@@ -0,0 +1,132 @@
+﻿using NAudio.Wave.SampleProviders;
+using NAudio.Wave;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace V0005
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        const int TotalEtapas = 10;
+
+        Dictionary<int, int> frequenciasBeep = new Dictionary<int, int>
+        {
+            { 0, 330 },
+            { 1, 352 },
+            { 2, 396 },
+            { 3, 495 }
+        };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InicializarSequencia();
+
+            DataContext = this;
+        }
+
+        void InicializarSequencia()
+        {
+            Sequencia = new int[TotalEtapas];
+
+            for (int indice = 0; indice < Sequencia.Length; indice += 1)
+            {
+                Sequencia[indice] = Random.Shared.Next(0, 4);
+            }
+        }
+
+        int[] sequencia = Array.Empty<int>();
+        public int[] Sequencia
+        {
+            get => sequencia;
+            set
+            {
+                sequencia = value;
+                OnPropertyChanged();
+            }
+        }
+
+        int numeroEtapasAlcancadas = 0;
+        public int NumeroEtapasAlcancadas
+        {
+            get => numeroEtapasAlcancadas;
+            set
+            {
+                numeroEtapasAlcancadas = value;
+                OnPropertyChanged();
+            }
+        }
+
+        bool vezDoGenius = true;
+        public bool VezDoGenius
+        {
+            get => vezDoGenius;
+            set
+            {
+                vezDoGenius = value;
+                OnPropertyChanged();
+            }
+        }
+
+        int posicaoNoVetor = 0;
+        public int PosicaoNoVetor
+        {
+            get => posicaoNoVetor;
+            set
+            {
+                posicaoNoVetor = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName]string propertyName = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            VezDoGenius = true;
+            await AplicarVezDoGeniusAsync();
+        }
+
+        async Task GerarBeepAsync(int sequencia)
+        {
+            SignalGenerator gen = new SignalGenerator()
+            {
+                Gain = 0.3,
+                Frequency = frequenciasBeep[sequencia],
+                Type = SignalGeneratorType.Sin,
+            };
+
+            using (WaveOutEvent wo = new WaveOutEvent())
+            {
+                wo.Init(gen.Take(TimeSpan.FromSeconds(1.5)));
+                wo.Play();
+                while (wo.PlaybackState == PlaybackState.Playing)
+                {
+                    await Task.Delay(100);
+                }
+            }
+        }
+
+        private async void BotaoGenius_Click(object sender, RoutedEventArgs e)
+        {
+            switch (VezDoGenius)
+            {
+                case true: 
+                    return;
+                default:
+                    break;
+            }
+
+            Button botao = (Button)sender;
+
+            await VerificarBotaoApertadoAsync(botao);
+        }
+    }
+}
